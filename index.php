f1c70bc85cc51226987e9f2ac9563b3a46a84ed4
komsak-i9 <komsak3_16@hotmail.com>
2018-05-14 11:59:52 +0700

    Add Add Code Receive Picture from User

--- index.php
+++ index.php
@@ -22,23 +22,20 @@
        foreach($events['events'] as $event){
            // Line API send a lot of event type, we interested in message only
            if($event['type']=='message'){
-              switch($event['message']['type']){
+              switch($event['message']['type'])
+              {
                 case 'text' :
                     // Get replyToken
                     $replyToken = $event['replyToken'];
                     // Reply Token
-                    $respMessage = 'Hello, your message is '.$event['message']['text'];
-                //     $httpClient = new CurlHTTPClient($channel_token);
-                //     $bot = new LINEBot($httpClient,array('channelSecret'=>$channel_secret));
-                //     $textMessageBuilder = new TextMessageBuilder($respMessage);
-                //     $response = $bot->replyMessage($replyToken,$textMessageBuilder);
+                    $respMessage = 'Hello, your message is '.$event['message']['text'];            
                     break;
-                // case 'image' :
-                //     $messageID = $event['message']['id'];
-                //     $respMessage = 'Hello Your image ID is '.$messageID;
-                //     break;
+                case 'image' :
+                    $messageID = $event['message']['id'];
+                    $respMessage = 'Hello Your image ID is '.$messageID;
+                    break;
                 default : 
-                    $respMessage = 'Please send text or image only';
+                    $respMessage = 'Please Send text or image only';
                     break;                  
               }              
               $httpClient = new CurlHTTPClient($channel_token);
