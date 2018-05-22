e83527cc289ff813eba6ed89801aedbbad1baaaf
komsak-i9 <komsak3_16@hotmail.com>
2018-05-22 12:18:26 +0700

    Add New file for Send VDO to Line API

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php
   require_once('./vendor/autoload.php');
-  //Namespace
+
+  // Namespace
   use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
   use \LINE\LINEBot;
-  use \LINE\LINEBot\MessageBuilder\StickerMessageBuilder;
+  use \LINE\LINEBot\MessageBuilder\VideoMessageBuilder;
 
   // Token
   $channel_token = 'WuQLwcvhUG82GtQK5belpZQJBJuBNR+G1JGGEoLbZjPWMiWMDOOysJRIveqGsW0YjuAPsXHJb0mnOpCs5IHmGG5pm4OSuGlGVIW7329WWnrA6zWyV5pbeFXdAZjeKjmwoCPWr+yZo8mbJE3mZ2IV/QdB04t89/1O/w1cDnyilFU=';
@@ -12,23 +13,25 @@
   $httpClient = new CurlHTTPClient($channel_token);
   $bot = new LINEBot($httpClient,array('channelSecret'=>$channel_secret));
 
-  //Get Message from Line API
-  $content = file_get_contents('php://input');
-  $events  = json_decode($content,true);
+  // Get Message from Line API
+  $content = file_get_contents('php://Input');
+  $events = json_decode($content,true);
 
-  if(!is_null($events['events'])){
-      // Loop through each event
-      foreach($events['events'] as $event){
-        // Get Reply Token 
-        $replyToken = $event['replyToken'];
+  if(!is_null($events['events']))
+  {
+     // Loop through each event
+     foreach($events['events'] as $event)
+     {
+          // Get Reply Token
+          $replyToken = $event['replyToken'];
 
-        //Sticker
-        $packageId = 1;
-        $stickerId = 116;
+          // Video
+          $originContentUrl = 'https://www.select2web.com.com/the-fuji.mp4';
+          $previewImageUrl = 'https://www.select2web.com.com/the-fuji.jpg';
 
-        $textMessageBuilder = new StickerMessageBuilder($packageId,$stickerId);
-        $response = $bot->replyMessage($replyToken,$textMessageBuilder);
-      }
+          $vdoMessageBuilder = new VideoMessageBuilder($originContentUrl,$previewImageUrl);
+          $response = $bot->replyMessage($replyToken,$vdoMessageBuilder);
+     }
   }
-  echo 'OK2';
-?>
+  echo 'OK';
+?> 
---
+++ index_SendStickerToLineAPI.php
@@ -0,0 +1,34 @@
+<?php
+  require_once('./vendor/autoload.php');
+  //Namespace
+  use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
+  use \LINE\LINEBot;
+  use \LINE\LINEBot\MessageBuilder\StickerMessageBuilder;
+
+  // Token
+  $channel_token = 'WuQLwcvhUG82GtQK5belpZQJBJuBNR+G1JGGEoLbZjPWMiWMDOOysJRIveqGsW0YjuAPsXHJb0mnOpCs5IHmGG5pm4OSuGlGVIW7329WWnrA6zWyV5pbeFXdAZjeKjmwoCPWr+yZo8mbJE3mZ2IV/QdB04t89/1O/w1cDnyilFU=';
+  $channel_secret = 'e6190998d06ed34a6540c014d384d350';
+
+  $httpClient = new CurlHTTPClient($channel_token);
+  $bot = new LINEBot($httpClient,array('channelSecret'=>$channel_secret));
+
+  //Get Message from Line API
+  $content = file_get_contents('php://input');
+  $events  = json_decode($content,true);
+
+  if(!is_null($events['events'])){
+      // Loop through each event
+      foreach($events['events'] as $event){
+        // Get Reply Token 
+        $replyToken = $event['replyToken'];
+
+        //Sticker
+        $packageId = 1;
+        $stickerId = 116;
+
+        $textMessageBuilder = new StickerMessageBuilder($packageId,$stickerId);
+        $response = $bot->replyMessage($replyToken,$textMessageBuilder);
+      }
+  }
+  echo 'OK2';
+?>
